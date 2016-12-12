7a4aa41da370ca163574f9dc23810ad4db59e3b7
canonxu <canonxu@sjtu.edu.cn>
2016-12-13 00:51:45 +0800

    install mysql

--- mysql/apt-mysql.sh
+++ mysql/apt-mysql.sh
@@ -6,11 +6,19 @@ install=$1
 
 if [ $1 = "install" ]; then
     echo "start installing mysql..."
-    apt-get install mysql-server
+    apt-get install mysql-server mysql-client
     echo "install mysql successfully"
 elif [ $1 = "uninstall" ]; then
     echo "start uninstalling mysql..."
     sudo apt-get remove mysql-server
+    sudo apt-get remove mysql-client
+    sudo apt-get remove mysql-common
+    sudo apt-get autoremove mysql-common
+    sudo apt-get remove mysql-community-server
+    sudo apt-get autoremove mysql-community-server
+    sudo apt-get remove mysql-community-client
+    #clean configs
+    dpkg -l|grrep ^rc|awk ‘{print $2}’|xargs dpkl --purge
     echo "uninstall mysql successfully"
 fi
 
---
+++ mysql/install_mysql.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -c http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.54-linux2.6-x86_64.tar.gz
