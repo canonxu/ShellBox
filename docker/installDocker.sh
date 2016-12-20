31c890d0b2c9d1e4de7e9631935f5912bcdcec5e
canonxu <canonxu@sjtu.edu.cn>
2016-12-21 04:01:10 +0800

    install mysql successfully

--- docker/installDocker.sh
+++ docker/installDocker.sh
@@ -3,3 +3,13 @@
 apt-get intall docker.io
 
 version=`docker version|head -1|cut -c 8-` && echo "docker $version installed successfully"
+
+# update to docker 1.9
+
+sh -c "echo 'deb https://apt.dockerproject.org/repo debian-$(lsb_release -sc) main' | cat > /etc/apt/sources.list.d/docker.list"
+
+apt-get purge docker.io
+apt-get update
+apt-get install docker-engine
+
+version=`docker version|sed -n '2p'` && echo "docker $version installed successfully"
--- mysql/install_mysql.sh
+++ mysql/install_mysql.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+cd /usr/local
 wget -c http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.54-linux2.6-x86_64.tar.gz
+tar xvf mysql-5.5.54-linux2.6-x86_64.tar.gz 
+mv mysql-5.5.54 mysql
+cd mysql
+
+# set auth
+
+scripts/mysql_install_db --user=mysql
+
+cp support-files/mysql.servier /etc/init.d/mysqld
+
+
+service mysqld start|stop|restart|status|reload|...
+
+# set root account of mysql
+bin/mysqladmin -u root password ***
+
+# set soft link for 'mysql'/'mysqladmin' command
+ln -s bin/mysql /usr/bin
+ln -s bin/mysqladmin /usr/mysqladmin
+
+# grant an account of mysql named 'canonxu' with password 'foobar'
+grant all privileges on *.* to canonxu@"%" identified by 'foobar' with grant option
+
